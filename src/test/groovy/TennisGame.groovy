df0f9634754936b2fc4057b1ad6de18b068bf13b
曾義格 <tsengeagle@tzuchi.com.tw>
2018-08-11 23:31:08 +0800

    tdd方式寫完，沒有重構

--- src/test/groovy/TennisGame.groovy
+++ src/test/groovy/TennisGame.groovy
@@ -1,6 +1,39 @@
 class TennisGame {
-    def score() {
-        return "Love All"
+    def player2 = 0
+    private Map scoreTable = [0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Fourty"]
+    def player1 = 0
 
+    def score() {
+        if (player1 == player2) {
+            if (player1 >= 3 || player2 >= 3) {
+return "Duece"
+            }
+            return "${scoreTable[player1]} All"
+        }
+        if (player1 > 0 || player2 > 0) {
+            if (player1 >= 3 && player2 >= 3) {
+                if (player1 - player2 > 1) {
+                    return "Player1 win"
+                }
+                if (player2 - player1 > 1) {
+                    return "Player2 win"
+                }
+                if (player1 > player2) {
+                    return "Player1 Adv"
+                } else {
+                    return "Player2 Adv"
+                }
+            }
+            return "${scoreTable[player1]} ${scoreTable[player2]}"
+        }
+        return "Love All"
+    }
+
+    def player2Score() {
+        player2++
+    }
+
+    def player1Score() {
+        player1++
     }
 }
--- src/test/groovy/TennisTests.groovy
+++ src/test/groovy/TennisTests.groovy
@@ -2,10 +2,151 @@ import spock.lang.Specification
 
 
 class TennisTests extends Specification {
+    private TennisGame tennisGame
 
-    def '0:0'() {
-        expect:
-        def tennisGame = new TennisGame()
-        tennisGame.score() == "Love All"
+    def setup() {
+        tennisGame = new TennisGame()
     }
+
+    def 'Love All'() {
+        expect:
+        this.tennisGame.score() == "Love All"
+    }
+
+    def 'Love Fifteen'() {
+        GivenPlayer2Score()
+        expect:
+        this.tennisGame.score() == "Love Fifteen"
+    }
+
+    def 'Love Thirty'() {
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        expect:
+        this.tennisGame.score() == "Love Thirty"
+    }
+
+    def 'Love Fourty'() {
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        expect:
+        this.tennisGame.score() == "Love Fourty"
+    }
+
+    def 'Fifteen Love'() {
+        GivenPlayer1Score()
+        expect:
+        this.tennisGame.score() == "Fifteen Love"
+    }
+
+    def 'Thirty Love'() {
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        expect:
+        this.tennisGame.score() == "Thirty Love"
+    }
+
+    def 'Fifteen All'() {
+        GivenPlayer1Score()
+        GivenPlayer2Score()
+        expect:
+        this.tennisGame.score() == "Fifteen All"
+    }
+
+    def 'Thirty All'() {
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        expect:
+        this.tennisGame.score() == "Thirty All"
+    }
+
+    def 'Duece 3:3'() {
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        expect:
+        this.tennisGame.score() == "Duece"
+    }
+
+    def 'Duece 4:4'() {
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer2Score()
+        expect:
+        this.tennisGame.score() == "Duece"
+    }
+
+    def 'Player1 adv 4:3'() {
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        expect:
+        this.tennisGame.score() == "Player1 Adv"
+    }
+
+    def 'Player2 adv 4:5'() {
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        expect:
+        this.tennisGame.score() == "Player2 Adv"
+    }
+
+    def 'Player1 win 5:3'() {
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        expect:
+        this.tennisGame.score() == "Player1 win"
+    }
+
+    def 'Player2 win 6:4'() {
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer2Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        GivenPlayer1Score()
+        expect:
+        this.tennisGame.score() == "Player2 win"
+    }
+
+    private void GivenPlayer1Score() {
+        tennisGame.player1Score()
+    }
+
+    private void GivenPlayer2Score() {
+        tennisGame.player2Score()
+    }
+
 }
